cefbd5e6a1e4cc2c0dddb53bef0d12515ea7a997
emilianogastaldo <emiliano0303@gmail.com>
2024-01-09 16:09:51 +0100

    added isPalindrome function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log("JS OK")
+/*
+    Palindorma:
+    -creo una flag booealana
+    -confronto la prima e ultima lettera della parola per vedere se sono uguali
+    -basta fare il confronto per metà della lunghezza
+    -!attenzione alle lunghezze dispari: meglio usare Math.floor
+    -se qualche lettera non coincide trasformo la flag di validazione
+    -ritorno la flag
+*/
+/** */
+function isPalindrome(word) {
+    //flag per sapere se è palindroma o meno
+    let isValid = false;
+
+    //Controllo le lettere
+    for (let i = 0; i < Math.floor(word.length / 2) && !isValid; i++) {
+        // if (word.charAt(i) === word.charAt(word.length - 1 - i)) isValid = true;
+        isValid = word.charAt(i) === word.charAt(word.length - 1 - i);
+    };
+    return isValid;
+};
+const input = prompt('Dammi una parola e ti dirò se è palindroma')
+
+console.log(isPalindrome(input));
